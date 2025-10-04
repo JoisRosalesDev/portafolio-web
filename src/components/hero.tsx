dd24b37b726c6ba95f652e4e80bbfa155c0c10aa
Jois Rosales Fernández <joisrosafer@gmail.com>
2025-10-03 22:02:26 -0300

    actualización de cv

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -64,12 +64,14 @@ export function HeroSectionOne() {
         >
           <a
             href="https://www.linkedin.com/in/jois-rosales-fernández-260baa24b"
+            target="_blank"
             className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200"
           >
             LinkedIn
           </a>
           <a
             href="https://github.com/JoisRosalesDev"
+            target="_blank"
             className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900"
           >
             GitHub
