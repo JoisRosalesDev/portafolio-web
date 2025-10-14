904a7a7788521ae31a2bc18d6e10a9946a6b21e8
Jois Rosales Fernández <joisrosafer@gmail.com>
2025-10-14 12:27:44 -0300

    Adición de Paws At Route

--- src/components/proyectos.tsx
+++ src/components/proyectos.tsx
@@ -63,7 +63,7 @@ const people = [
 export function ThreeDCardDemo() {
   return (
     <>
-      <div id="proyectos" className="grid lg:grid-cols-2 max-w-6xl mx-auto">
+      <main id="proyectos" className="grid lg:grid-cols-2 max-w-6xl mx-auto">
         <div>
           <CardContainer className="inter-var">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
@@ -71,7 +71,55 @@ export function ThreeDCardDemo() {
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                YeezyVerse
+                Paws At Route <span className="text-xs text-gray-300">2025 (En desarrollo)</span>
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 text-start"
+              >
+                Aplicación web que conecta a dueños de mascotas con paseadores verificados.
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <img
+                  src="/img/paws-cover.webp"
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt="thumbnail"
+                />
+              </CardItem>
+              <div className="flex justify-between items-center mt-20">
+                <CardItem
+                  translateZ={20}
+                  as="a"
+                  href="https://paws-at-route.vercel.app/"
+                  target="__blank"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  ir a la web →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as="a"
+                  href="https://github.com/vMattS/PawsAtRoute"
+                  target="__blank"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  Repositorio
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+        </div>
+        <div>
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                YeezyVerse <span className="text-xs text-gray-300">2025 (Febrero)</span>
               </CardItem>
               <CardItem
                 as="p"
@@ -120,7 +168,7 @@ export function ThreeDCardDemo() {
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                My Condition
+                My Condition <span className="text-xs text-gray-300">2025 (Enero)</span>
               </CardItem>
               <CardItem
                 as="p"
@@ -162,7 +210,7 @@ export function ThreeDCardDemo() {
             </CardBody>
           </CardContainer>
         </div>
-      </div>
+      </main>
     </>
   );
 }
